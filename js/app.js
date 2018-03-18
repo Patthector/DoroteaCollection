44ce21dc03cde8632d1ba55ec00d72cc673569c4
Pattor <pa77or@gmail.com>
2018-03-17 23:23:52 -0400

    Finishing the last view (1500px) and the static layout (1875px). Getting ready for compass.

--- js/app.js
+++ js/app.js
@@ -13,30 +13,42 @@ $(window).ready(function() {
     for (let i = 0; i < 6; i++) {/*just the first five elements*/
       ///SETTING THE HEIGHTS OF ALL THE IMGS
       height = 0;
-      if(children.eq(i).hasClass('collage__title-container') || children.eq(i).hasClass('collage__content-container')){
-        height = children.eq(i).children().eq(0).height() + 20; /*+20px's*/
-        height += 4; /*margin*/
-      }
-      else if(children.eq(i).hasClass('collage__verse-container')){
-        height = children.eq(i).children().eq(1).height();
-        height += children.eq(i).children().eq(0).height() * 2 + 32 * 2; /*top-line and bottom-line && margins*/
-        height += 4; /*margin*/
-      }
-      else{
-        height = children.eq(i).children().eq(0).height() + 4;/*img*//*margin*/
-        console.log(children.eq(i).children().eq(0).height());
+      if(children.eq(i).hasClass('collage__content-container')){
+        height = children.eq(i).children().eq(0).height() + 20; //+20px's
       }
 
-      let repetitions = Math.ceil(height / 1);/*10px*/
+      else if(children.eq(i).hasClass('collage__verse-container')){
+        height = children.eq(i).children().eq(1).height();
+        height += children.eq(i).children().eq(0).height() * 2 + 24 * 2; //top-line and bottom-line && margins
+
+      }/*
+      else{
+        height = children.eq(i).children().eq(0).height() + 4;//img*margin
+        console.log(children.eq(i).children().eq(0).height());
+      }*/
+      else{
+        height = children.eq(i).children().eq(0).height();
+      }
+      let repetitions = Math.ceil(height / 10);/*10px*/
       children.eq(i).css(`grid-row-end`,`span ${repetitions}`);
+      children.eq(i).css(`height`,repetitions * 10);//div's height
+      //SQUARE
       if(children.eq(i).hasClass('square')){
         $('.square').css(`grid-row-end`,`span ${repetitions}`);
+        $('.square').css(`height`,repetitions * 10);
+        $('.square img').css(`height`,'100%');//img's height
       }
+      //HRECT
       else if(children.eq(i).hasClass('hrect')){
         $('.hrect').css(`grid-row-end`,`span ${repetitions}`);/*horizontal rectangle*/
+        $('.hrect').css(`height`,repetitions * 10);
+        $('.hrect img').css(`height`,'100%');//img's height
       }
+      //VRECT
       else if(children.eq(i).hasClass('vrect')){
         $('.vrect').css(`grid-row-end`,`span ${repetitions}`);/*horizontal rectangle*/
+        $('.vrect').css(`height`,repetitions * 10);
+        $('.vrect img').css(`height`,'100%');//img's height
       }
     }
   }
